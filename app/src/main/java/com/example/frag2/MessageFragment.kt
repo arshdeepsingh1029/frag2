9b33be7f226e70a71934e5e37921106227eb3792
Arshdeep <arshdeepsingh95@gmail.com>
2022-10-14 14:06:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/frag2/MessageFragment.kt
@@ -0,0 +1,27 @@
+package com.example.frag2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+
+class MessageFragment : Fragment() {
+
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view2 = inflater.inflate(R.layout.fragment_message, container, false)
+
+        val backButton = view2.findViewById<Button>(R.id.back)
+
+        backButton.setOnClickListener {
+            view2.findNavController().navigate(R.id.action_messageFragment_to_welcomeFragment)
+        }
+
+        return view2
+    }
+
+}
